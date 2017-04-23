a51412e3ee9bc8642f26deab82604e102c9465eb
gamesukimanIRS <gamesukiman@yahoo.co.jp>
2017-04-23 14:13:14 +0900

    Update Main.php

--- src/xyzplugin/Main.php
+++ src/xyzplugin/Main.php
@@ -13,7 +13,9 @@ use pocketmine¥command¥CommandSender;
 class Main extends PluginBase implements Listener{
   
   public function onEnable(){
-    $this->getlogger()->info("xyzpluginを読み込みました");
+    $this->getlogger()->info("xyzpluginを読み込みました。作者:gamesukimanIRS");
+    $this->getlogger()->warning("製作者偽りと二次配布、改造、改造配布はおやめ下さい。");
+    $this->getlogger()->info("このプラグインを使用する際はどこかにプラグイン名「xyzplugin」と作者名「gamesukimanIRS」を記載する事を推奨します。")
   }
    
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
@@ -24,9 +26,9 @@ class Main extends PluginBase implements Listener{
         $x = $player->getX();
         $y = $player->getY();
         $z = $player->getZ();
-        $sender->sendMessage("あなたの座標はX=".x."、Y=".y."、Z=".z."、ワールド=".l."です。");
+        $sender->sendMessage("§b[XYZ]§fあなたの座標はX=".x."、Y=".y."、Z=".z."、ワールド=".l."です。");
       }else{
-        $sender->sendMesaage("サーバー内で実行して下さい");
+        $this->getlogger()->warning("サーバー内で実行して下さい");
       }
     }
   }
