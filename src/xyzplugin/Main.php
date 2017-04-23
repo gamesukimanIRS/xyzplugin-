3f193863f60f16c0c9ee4ed60f657b19fd182431
gamesukimanIRS <gamesukiman@yahoo.co.jp>
2017-04-23 17:11:00 +0900

    Update Main.php

--- src/xyzplugin/Main.php
+++ src/xyzplugin/Main.php
@@ -11,19 +11,21 @@ use pocketmine¥command¥Command;
 use pocketmine¥command¥CommandSender;
 use pocketmine¥Server;
 use pocketmine¥plugin¥PluginBase;
+use pocketmine¥event¥Listener;
 
 class Main extends PluginBase implements Listener{
   
   public function onEnable(){
     $this->getlogger()->info("xyzpluginを読み込みました。作者:gamesukimanIRS");
     $this->getlogger()->warning("製作者偽りと二次配布、改造、改造配布はおやめ下さい。");
-    $this->getlogger()->info("このプラグインを使用する際はどこかにプラグイン名「xyzplugin」と作者名「gamesukimanIRS」を記載する事を推奨します。")
+    $this->getlogger()->info("このプラグインを使用する際はどこかにプラグイン名「xyzplugin」と作者名「gamesukimanIRS」を記載する事を推奨します。");
   }
    
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     if($command->getName() == "xyz"){
       if($sender instanceof Player){
         $sender->getName();
+        $level->getFolderName();
         $l = $sender->getLevel();
         $x = $sender->getX();
         $y = $sender->getY();
