a14fafa3eaa9df72697c173916c17d43d01d7b53
gamesukimanIRS <gamesukiman@yahoo.co.jp>
2017-04-23 11:06:52 +0900

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,1 +1,16 @@
+<?php
 
+/*hajime te no orijinaru plugin
+* (c)gamesukimanIRS
+*/
+
+namespace Xyzplugin;
+
+use pocketmine¥Player;
+use pocketmine¥command¥Command;
+use pocketmine¥command¥CommandSender;
+
+class Main extends PluginBase implements Listener{
+  
+
+？>
