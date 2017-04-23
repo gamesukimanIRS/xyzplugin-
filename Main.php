b381ede6669b811e28c5f0ad402ec170928dfd73
gamesukimanIRS <gamesukiman@yahoo.co.jp>
2017-04-23 13:23:03 +0900

    Update Main.php

--- Main.php
+++ Main.php
@@ -15,23 +15,20 @@ class Main extends PluginBase implements Listener{
   public function onEnable(){
     $this->getlogger()->info("xyzpluginを読み込みました");
   }
-  
+   
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-    
-  switch ($args[0])
-    case 'xyz':
-    if($player instanceof Player){
-      $sender->getName();
-      $l = $player->getLevel();
-      $x = $player->getX();
-      $y = $player->getY();
-      $z = $player->getZ();
-      $sender->sendMessage("あなたの座標はX=".x."、Y=".y."、Z=".z."、ワールド=".l."です。");
-      
-    }else{
-  $sender->sendMesaage("サーバー内で実行して下さい");
+    if($command->getName() == "xyz"){
+      if($sender instanceof Player){
+        $sender->getName();
+        $l = $player->getLevel();
+        $x = $player->getX();
+        $y = $player->getY();
+        $z = $player->getZ();
+        $sender->sendMessage("あなたの座標はX=".x."、Y=".y."、Z=".z."、ワールド=".l."です。");
+      }else{
+        $sender->sendMesaage("サーバー内で実行して下さい");
+      }
     }
-    break;
         
   
 
